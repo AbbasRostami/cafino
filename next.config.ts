eadcb80164dd7eaaae1a533a7381c12425be0adf
Abbas Rostami <abbas.cracker@gmail.com>
2025-07-13 02:41:06 +0330

    Implement SVG handling in webpack configuration
    - Adds custom webpack configuration to handle SVG files differently based on resource queries.
    - Configures SVG files to be processed by @svgr/webpack for component usage while maintaining existing file loader functionality.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
     remotePatterns: [
       {
@@ -23,6 +22,32 @@ const nextConfig: NextConfig = {
         pathname: "/**",
       },
     ],
+  },
+
+  webpack(config) {
+    const fileLoaderRule = config.module.rules.find((rule: any) =>
+      rule.test?.test?.(".svg")
+    );
+
+    config.module.rules.push(
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/,
+      },
+      {
+        test: /\.svg$/i,
+        issuer: /\.[jt]sx?$/,
+        resourceQuery: { not: [/url/] },
+        use: ["@svgr/webpack"],
+      }
+    );
+
+    if (fileLoaderRule) {
+      fileLoaderRule.exclude = /\.svg$/i;
+    }
+
+    return config;
   },
 };
 
