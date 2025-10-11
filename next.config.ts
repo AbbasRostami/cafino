5ac4df2f03b0e1b141624a9f0abaa8860485a47f
Abbas Rostami <abbas.cracker@gmail.com>
2025-10-11 23:04:45 +0330

    refactor(next.config): remove deprecated security headers for cleaner configuration
    - Eliminated unnecessary security headers from next.config.ts to streamline the configuration.
    - Focused on maintaining essential headers while improving overall clarity and maintainability of the file.

--- next.config.ts
+++ next.config.ts
@@ -23,31 +23,6 @@ const nextConfig: NextConfig = {
   async headers() {
     return [
       {
-        source: "/(.*)",
-        headers: [
-          { key: "X-Content-Type-Options", value: "nosniff" },
-          { key: "X-Frame-Options", value: "DENY" },
-          { key: "Referrer-Policy", value: "strict-origin-when-cross-origin" },
-          {
-            key: "Permissions-Policy",
-            value: "camera=(), microphone=(), geolocation=()",
-          },
-          {
-            key: "Content-Security-Policy",
-            value: `
-              default-src 'self';
-              script-src 'self' 'unsafe-inline' 'unsafe-eval' https://cafino.storage.c2.liara.space https://api.cafinoo.com;
-              style-src 'self' 'unsafe-inline';
-              img-src 'self' data: blob: https://cafino.storage.c2.liara.space;
-              connect-src 'self' https://api.cafinoo.com https://cafino.storage.c2.liara.space;
-              font-src 'self' data:;
-              frame-ancestors 'none';
-            `.replace(/\n/g, ""),
-          },
-        ],
-      },
-
-      {
         source: "/sw.js",
         headers: [
           {
