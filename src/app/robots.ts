0dc2216ba0bf86902d2097841576ab9095a7e831
Abbas Rostami <abbas.cracker@gmail.com>
2025-11-15 17:23:19 +0330

    refactor(robots): update robots rules for improved SEO and site structure
    - Changed the format of the robots rules from an object to an array for better organization.
    - Updated allow and disallow paths to enhance site crawling and indexing.
    - Ensured compatibility with the new dynamic sitemap generation.

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -2,11 +2,19 @@ import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-      disallow: ["/_next/", "/admin/", "/private/", "/api/"],
-    },
+    rules: [
+      {
+        userAgent: "*",
+        allow: ["/", "/_next/static/"],
+        disallow: [
+          "/_next/image/",
+          "/_next/data/",
+          "/admin/",
+          "/private/",
+          "/api/",
+        ],
+      },
+    ],
     sitemap: "https://cafino.site/sitemap.xml",
   };
 }
